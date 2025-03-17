f7248c2d09b8c1ed8ad2cf599c03474bfbc13f11
Hardik <hardikdwivedi007@gmail.com>
2025-03-17 14:07:09 +0530

    End of Section

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,12 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API;
+namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]//  /api/users
-public class UsersController(DataContext context) : ControllerBase{
+public class UsersController(DataContext context) : BaseApiController{
     // private readonly DataContext _context;
 
     // public UsersController(DataContext context)
@@ -15,6 +14,7 @@ public class UsersController(DataContext context) : ControllerBase{
     //     _context = context;
     // }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
         var users =await context.Users.ToListAsync();
@@ -22,6 +22,7 @@ public class UsersController(DataContext context) : ControllerBase{
         //return Ok(users);
     }
 
+    [Authorize]
     [HttpGet("{id:int}")] //  /api/users/2
     public async Task<ActionResult<AppUser>> GetUser(int id){
         var user = await context.Users.FindAsync(id);
--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,46 @@
+using System.Text;
+using API;
 using API.Data;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+//builder.Services.AddControllers();
+//builder.Services.AddDbContext<DataContext>(opt =>
+//{
+  //  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
+//builder.Services.AddCors();
+//builder.Services.AddScoped<ITokenService, TokenService>();
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options=>{
+//         var tokenKey = builder.Configuration["TokenKey"]??throw new Exception("TokenKey not found");
+//         options.TokenValidationParameters=new TokenValidationParameters{
+//             ValidateIssuerSigningKey=true,
+//             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+//             ValidateIssuer = false,
+//             ValidateAudience = false
+//         };
+//     });
+//Commented as now implemented in extensions
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
