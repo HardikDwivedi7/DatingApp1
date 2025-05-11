745220d62c8eea932435876a24b57d9b291b1969
Hardik <hardikdwivedi007@gmail.com>
2025-05-11 21:10:33 +0530

    End of Section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,33 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { NgIf } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router)
+  private toaster = inject(ToastrService);
   // loggedIn = false;
   model: any = {};
 
   login() {
     // console.log(this.model);
     this.accountService.login(this.model).subscribe({
-      next: response =>{
-        console.log(response);
+      // next: response =>{
+      next: _ =>{
+        this.router.navigateByUrl("/members")
+        // console.log(response);
         // this.loggedIn = true;
       },
-      error: error=>console.log(error)
+      // error: error=>console.log(error)
+      error: error=>this.toaster.error(error.error)
     })
   }
   logout(){
     // this.loggedIn=false;
     this.accountService.logout();
+    this.router.navigateByUrl("/");
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toaster = inject(ToastrService);
   // @Input() usersFromHomeComponent: any; old approach before angular 17.3
   // usersFromHomeComponent= input.required<any>()
   // @Output() cancelRegister= new EventEmitter();
@@ -24,7 +26,8 @@ export class RegisterComponent {
         console.log(response);
         this.cancel();
       },
-      error: error=>console.log(error)
+      // error: error=>console.log(error)
+      error: error=>this.toaster.error(error.error)
     })
   }
 
