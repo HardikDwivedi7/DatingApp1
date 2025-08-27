4eed911a91d6ca37a1a62d6bfad7bd0a0b06b30b
Hardik <hardikdwivedi007@gmail.com>
2025-08-28 03:14:31 +0530

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -3,10 +3,17 @@ using System.ComponentModel.DataAnnotations;
 namespace API;
 
 public class RegisterDto{
+    // [Required]
+    // [MaxLength(100)]
+    // public required string Username { get; set; }
+    
+    // [Required]
+    // public required string Password { get; set; }
     [Required]
     [MaxLength(100)]
-    public required string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
     
     [Required]
-    public required string Password { get; set; }
-}
+    [StringLength(8, MinimumLength =4)]
+    public string Password { get; set; } = string.Empty;
+ }
